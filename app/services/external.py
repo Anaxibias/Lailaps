3cb9a2a646346266055bf44aa6e422813f0a2e04
Anaxibias <chris.g.farnsworth@gmail.com>
2026-07-24 18:04:38 -0700

    increased accuracy and success rate of auto populate for url entries and job details entry via fallback queries for urls and integrating Gemini and Serper for obtaining job info based on urls

--- app/services/external.py
+++ app/services/external.py
@@ -42,7 +42,7 @@ def check_blocklist(job_url):
 
     return False
         
-def query_gemini(query):
+def query_gemini(query, output_type):
     client = genai.Client()
 
     interaction = client.interactions.create(
@@ -51,39 +51,14 @@ def query_gemini(query):
         tools=[{"type": "google_search"}]
     )
 
-    response_json = interaction.output_text
-    return response_json.loads(response_json)
+    response = interaction.output_text
 
-def fetch_job_details(job_url):
+    if output_type == "json":
+        return json.loads(response)
+    else:
+        return response
 
-    # search_results = DDGS().text(job_url, max_results=1)
-
-    # if len(search_results) < 1:
-    #     return "unknown", "unknown"
-
-    # result_headline = search_results[0]["title"]
-    # result_body = search_results[0]["body"]
-
-    query = f"""Extract the hiring company and the job title from the search results for this url
-                
-                url: {job_url}.
-
-                Return strictly a JSON object in this format: {{"company": "company-name", "job_title": "job-title"}}. Output raw JSON only. Do not include markdown formatting, backticks, or any additional text."""
-
-    response = query_gemini(query)
-    
-    company = response['company']
-    job_title = response['job_title']
-    
-    return job_title, company
-
-def fetch_job_url(job_title, company):
-
-    allowlist_string = " OR site:".join(allowlist)
-
-    search_string = f'''"{job_title}" "{company}" (site:{allowlist_string})'''
-
-    company, job_title = company.lower(), job_title.lower()
+def call_serper(search_string):
 
     conn = http.client.HTTPSConnection("google.serper.dev")
     payload = json.dumps({
@@ -98,7 +73,67 @@ def fetch_job_url(job_title, company):
     res = conn.getresponse()
     data = res.read().decode("utf-8")
     conn.close()
-    json_response = json.loads(data)
+    return json.loads(data)
+
+def fetch_job_details(job_url):
+
+    # search_results = DDGS().text(job_url, max_results=1)
+
+    # if len(search_results) < 1:
+    #     return "unknown", "unknown"
+
+    # result_headline = search_results[0]["title"]
+    # result_body = search_results[0]["body"]
+
+    query = f'''Convert this messy job URL into a highly targeted Google Search query designed to find the original, canonical job posting.
+
+                URL: {job_url}
+                Requirements:
+
+                    Extract the likely company name from the subdomain (e.g., 'acme' in acme.greenhouse.io) or the path.
+                    Extract the slugified job title and replace hyphens/underscores with spaces.
+                    Extract any unique requisition IDs or job numbers.
+                    Discard all tracking parameters (e.g., utm_source, gh_src, session_id).
+                    Format the output using Google Search operators where appropriate. For example, use quotes around the company name and title, or include the ATS domain.
+                    Do not include the original tracking URL in the query.
+                    Do not attempt to access the url.
+    
+                Return ONLY the raw search query string. Do not include markdown formatting, JSON, or any conversational text.'''
+
+    search_string = query_gemini(query, "string")
+    json_response = call_serper(search_string)
+
+    for obj in json_response.get("organic", []):
+        headline = obj["title"]
+        snippet = obj["snippet"]
+        if obj["position"] == 1:
+            break
+
+    query = f'''Extract the hiring company and the job title from this search result headline and description snippet:
+
+
+                headline: {headline}
+                snippet: {snippet}
+
+                Return strictly a JSON object in this format {{"company": "company-name", "job_title": "job-title"}}'''
+
+    response = query_gemini(query, "json")
+    
+    company = response.get('company') or ''
+    job_title = response.get('job_title') or ''
+    
+    return job_title, company
+
+def fetch_job_url(job_title, company):
+
+    allowlist_string = " OR site:".join(allowlist)
+
+    search_string = f'''"{job_title}" "{company}" (site:{allowlist_string})'''
+
+    company = company.lower()
+    job_title = job_title.lower()
+
+    json_response = call_serper(search_string)
 
     for obj in json_response.get("organic", []):
         title = obj["title"]
@@ -121,7 +156,23 @@ def fetch_job_url(job_title, company):
         company_match_score_headline = fuzz.partial_ratio(company, title)
         title_match_score_headline = fuzz.token_set_ratio(job_title, title)
 
-        if (company_match_score_url > 50 or company_match_score_headline > 50) and title_match_score_headline > 50:
+        if (company_match_score_url > 40 or company_match_score_headline > 50) and title_match_score_headline > 50:
+            return link
+
+    search_string = f'''"{job_title}" "{company}" "jobs"'''
+
+    json_response = call_serper(search_string)
+
+    for obj in json_response.get("organic", []):
+        title = obj["title"].lower()
+        link = obj["link"]
+
+        company_match_score_url = fuzz.partial_ratio(company, link.lower())
+        company_match_score_headline = fuzz.partial_ratio(company, title)
+        title_match_score_headline = fuzz.token_set_ratio(job_title, title)
+
+        if (company_match_score_headline > 60 or company_match_score_url > 60) and title_match_score_headline > 60:
+
             return link
 
     return ""
